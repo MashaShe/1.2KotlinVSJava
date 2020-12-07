6021399bb88d3e54b24600a26c80a282fcd6088b
MashaShe <shabalkova@gmail.com>
2020-12-07 13:52:06 +0300

    /task 1.2.2 updated

--- src/main/kotlin/1.2.2DeclensionOfNoun.kt
+++ src/main/kotlin/1.2.2DeclensionOfNoun.kt
@@ -1,6 +1,6 @@
 fun numberOfLikes() {
-    val likes = 11;
-    val message = if (likes % 10 == 1 && likes !== 11) "человеку" else "людям"
+    val likes = 23101;
+    val message = if (likes % 10 == 1 && likes % 100 !== 11) "человеку" else "людям"
     println("Домашнее задание 1.2.2 - Люди/Человеки")
     println("Понравилось $likes $message \n")
 }
